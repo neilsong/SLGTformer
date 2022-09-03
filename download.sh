13aa42dff335e5b054b786d622b6f2df305d8ddc
Neil Song <song.neil.song@gmail.com>
2022-09-03 13:42:09 -0500

    init repo

--- download.sh
+++ download.sh
@@ -5,3 +5,6 @@ wget --no-check-certificate -r "https://drive.google.com/uc?export=download&id=1
 wget --no-check-certificate -r "https://drive.google.com/uc?export=download&id=1GJJROIvHpX90kCBTRMfK2xdIk85S7IGI&confirm=t" -O "data/train_label.pkl" 
 wget --no-check-certificate -r "https://drive.google.com/uc?export=download&id=1HsGKDepTEKVK7B9VBHmzLzGw_C9nb-oL&confirm=t" -O "data/val_data_joint.npy"
 wget --no-check-certificate -r "https://drive.google.com/uc?export=download&id=1NgH_NL3sjcYApmkY02UAPZ77HLMzezu_&confirm=t" -O "data/val_label.pkl"
+
+mkdir save_models
+mkdir output
